2d83265dd04ed28582526c13ae1e2caf644a7458
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-21 16:01:08 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point.
+*main: Prints either is positive, negative or zero.
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+int letter;
+/* your code goes there */
+for (letter = 97; letter <= 122; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
