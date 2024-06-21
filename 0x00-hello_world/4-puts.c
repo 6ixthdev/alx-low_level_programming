45dccf5a86d65dd636b8ccb9a5ce1741fea78213
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-20 17:11:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Entry point of the program.
+ * Prints the specified string using the puts function.
+ * Returns: 0 to indicate successful execution.
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
- return 0;
+return (0);
 }
