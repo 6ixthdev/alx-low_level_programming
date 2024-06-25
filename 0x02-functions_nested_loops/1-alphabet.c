83a63c0644ce6ce2780e82d07888185395efd59f
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-25 11:05:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet: funtion prints out the alphabet
+ * Return: Always 0 success
+ */
+
+void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+}
+
