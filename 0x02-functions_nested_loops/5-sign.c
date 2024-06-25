31951acd7fe8aacea689e846b3bbbaa820f4da71
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-25 14:34:40 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_sign - print on sign based on result
+ *@n: the character to print
+ *
+ * Return: void
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return -1;
+	}
+
+}
+
