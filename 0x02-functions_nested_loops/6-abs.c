24d59439576d4bee858e0d8ae9678661ecd73ced
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-25 16:09:58 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - prints the absolute number
+ *@num: the character to print
+ *
+ * Return: void
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		num = -num;
+	}
+
+	return (num);
+
+}
+
