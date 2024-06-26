f34e50d7c77dd04df414e4746feca6cf87795a13
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-25 18:19:16 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *times_table - prints the times table
+ *@void: returns no value
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int sum;
+	int i;
+	int a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a = 0; a <= 9; a++)
+		{
+			sum = i * a;
+
+			if (sum >= 10)
+			{
+				_putchar('0' + (sum % 10));
+			}
+			_putchar('0' + (sum % 10));
+
+			if (a < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (sum < 10)
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
+
