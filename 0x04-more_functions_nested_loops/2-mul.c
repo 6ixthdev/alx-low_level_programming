9837a541b747544a2c2f0645ee0675d19af4ce4f
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-27 10:42:48 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ * mul: multiplies two numbers
+ * @a: first argument provided
+ * @b: second argument provided
+ *
+ * Return: returns the value of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
