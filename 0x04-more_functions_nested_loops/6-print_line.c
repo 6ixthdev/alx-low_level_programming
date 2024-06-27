bf27dc55224ebf1c734d714466fab51ff0691797
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-06-27 11:45:05 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - the function prints lines
+ * print_line: the function prints lines
+ * @n: argument that determines how many to be printed
+ *
+ * Return: Always 1 on success
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
