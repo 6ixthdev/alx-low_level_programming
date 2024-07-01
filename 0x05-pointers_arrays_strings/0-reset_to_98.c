eb7ce93a023afbe5be33aff3a42d366a18e32361
Nnaemeka Odiegwu <nnaemekaodiegwu@gmail.com>
2024-07-01 14:29:57 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * reset_to_98: updates the value it points to to 98
+ * @n: argument taken by the functioin
+ *
+ * Return: Always 1 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
